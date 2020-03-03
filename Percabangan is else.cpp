58a5d42881492666bda75b57776ee9534f3a7bbe
andiyoga <61462077+andiyoga@users.noreply.github.com>
2020-03-03 21:55:42 +0700

    Add files via upload

---
+++ Percabangan is else.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+int nilai ;
+cout << "berapa nilai kamu" ;
+cin>> nilai;
+
+if(nilai>85) {
+	cout << "selamat anda lulus";
+} (nilai < 70);
+	cout << "maaf anda tidak lulus" ;
+}
+
